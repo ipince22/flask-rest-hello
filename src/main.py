d2b267b5cee9580102f50b53ca2c0aedaeef39e7
Daniel Ipince <ipince22@gmail.com>
2020-11-08 05:22:07 +0000

    nov8

--- src/main.py
+++ src/main.py
@@ -33,11 +33,44 @@ def sitemap():
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    #response_body = {
+    #    "msg": "Hello, this is your GET /user response "
+    #}
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
+    return jsonify(all_users), 200
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def create_user():
+    request_body = request.get_json()
+    user1 = User(email=request_body["email"],password=request_body["password"])
+    db.session.add(user1)
+    db.session.commit()
+    return jsonify(request_body), 200
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    request_body = request.get_json()
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    if "email" in request_body:
+        user1.email = request_body["email"]
+    db.session.commit()
+    
+    return jsonify(request_body), 200
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+    
+    return jsonify("OK!"), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
